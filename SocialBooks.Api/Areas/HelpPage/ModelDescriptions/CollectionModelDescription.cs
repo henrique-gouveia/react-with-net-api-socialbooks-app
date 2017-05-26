4a19a0951e51794e33734b1cdc07f01cf3ab94ec
Henrique Gouveia <henrique.gouveia17@gmail.com>
2017-05-26 18:52:04 -0300

    Add project files.

---
+++ SocialBooks.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace SocialBooks.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ SocialBooks.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SocialBooks.Api.WebApiApplication" Language="C#" %>
