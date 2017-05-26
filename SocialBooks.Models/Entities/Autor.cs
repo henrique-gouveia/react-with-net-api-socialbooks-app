52e10054888246ae35d4bfbcdd6c828db8f53718
Henrique Gouveia <henrique.gouveia17@gmail.com>
2017-05-26 20:05:56 -0300

    adicionado entidade Autor

---
+++ SocialBooks.Models/Entities/Autor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SocialBooks.Models.Entities
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DtCadastro { get; set; }
+    }
+}
