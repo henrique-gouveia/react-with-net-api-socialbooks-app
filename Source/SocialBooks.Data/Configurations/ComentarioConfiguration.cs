3d5003b5956a6a7fcc966fc99380f0dbfecc18b8
Henrique Gouveia <henrique.gouveia17@gmail.com>
2017-06-05 12:21:39 -0300

    adicionado estrutura base para SocialBooks Client
    ajustes na entidade comentario e configuração para o entityframework

---
+++ Source/SocialBooks.Data/Configurations/ComentarioConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using SocialBooks.Models.Entities;
+using System;
+using System.Data.Entity.ModelConfiguration;
+
+namespace SocialBooks.Data.Configurations
+{
+    public class ComentarioConfiguration : EntityTypeConfiguration<Comentario>
+    {
+        public ComentarioConfiguration()
+        {
+            HasKey(c => c.Id);
+
+            Property(c => c.Descricao)
+                .HasMaxLength(250);
+
+            Property(c => c.DtCadastro)
+                .HasColumnName("Data_Cadastro");
+
+            HasRequired(c => c.Livro)
+                .WithMany()
+                .HasForeignKey(c => c.LivroId);
+        }
+    }
+}
---
+++ Source/SocialBooks.Models/Entities/Comentario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SocialBooks.Models.Entities
+{
+    public class Comentario
+    {
+        public Comentario()
+        {
+            DtCadastro = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+
+        public int LivroId { get; set; }
+
+        [IgnoreDataMember]
+        public Livro Livro { get; set; }
+
+        public string Descricao { get; set; }
+
+        public DateTime DtCadastro { get; set; }
+        
+
+    }
+}
